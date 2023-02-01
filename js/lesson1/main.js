15f811deb6589f100a95151f22e1cd4b472f7ffa
sstanislaur <lisnychukstas@gmail.com>
2023-01-31 20:49:53 -0800

    1

---
+++ js/lesson1/main.js
@@ -0,0 +1,38 @@
+let a = 'hello';
+let b = 'owu.com.ua';
+console.log(a, b)
+
+let c
+c = [1, 10, -999, 123, 3.14, 2.7, 16];
+
+console.log(c)
+let d = true;
+console.log(d)
+let s = false;
+console.log(s)
+
+
+let firstname = 'Lisnychuk';
+let middlename = 'Stas';
+let lastname = 'Mykolayovych'
+let person = ` ${firstname} ${middlename} ${lastname}`;
+console.log(person)
+
+
+// object
+let person1 = [
+     firstname1 = 'Lisnychuk',
+     middlename1 = 'Stas',
+    lastname1 = 'Mykolayovych'
+]
+console.log(person1);
+
+
+// typeof
+let q = 100;
+console.log(typeof q);
+let w = '100';
+console.log(typeof w);
+let e = true;
+console.log(typeof e);
+
